72b15e144145fa99f7ac3080f7ab5549527258cc
Dev <kanuj2151@gmail.com>
2023-08-25 12:06:33 +0530

    sprt cls

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,8 @@
 package com.driver;
-public class A {
-	public String meth(){
-		return "Invoking method from class A";
-	}
+class A {
+
 }
-public class B extends A{
+class B extends A{
 	public String meth(){
 
 		return "Method is overridden in Extendend class B";
