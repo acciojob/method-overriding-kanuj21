b4867a74566686def8a057550c74e0942ea4ed3c
Dev <kanuj2151@gmail.com>
2023-08-25 10:15:54 +0530

    change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,5 @@
 package com.driver;
-class A{
-	  public String meth(){
-	    return "Invoking method from class A"; 
-	}
-}
+
 
 class B extends A {
 	public String meth(){
@@ -14,6 +10,7 @@ class B extends A {
 	public static void main(String [] args){
 		B obj = new B();
 		System.out.println(obj.meth());
+
 	}
   
 }
