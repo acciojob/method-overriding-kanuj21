154810f8c245d1eafc5b68428619eb7ff4a5be5d
Dev <kanuj2151@gmail.com>
2023-08-25 10:48:41 +0530

    ccc

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,11 +1,7 @@
 package com.driver;
 
 
-class B extends A {
-	public String meth(){
-		return "Method is overridden in Extendend class B";
-	}
-
+class Main {
 
 	public static void main(String [] args){
 		B obj = new B();
