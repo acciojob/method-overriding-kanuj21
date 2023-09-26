6678fdcf29765a0af391d1d75716387332611581
Dev <kanuj2151@gmail.com>
2023-09-26 15:30:50 +0530

    remove class files

--- src/main/java/com/driver/B.java
+++
@@ -1,4 +0,0 @@
-package com.driver;
-
-public class B extends A{
-}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,11 +1,24 @@
 package com.driver;
 
+class A {
+	public String meth(){
+		return "Invoking method from class A";
+	}
+}
+
+class B extends A{
+
+	public String meth(){
+		return "Method is overridden in Extendend class B";
+	}
+}
 
 class Main {
 
 	public static void main(String [] args){
 		B obj = new B();
 		System.out.println(obj.meth());
+        
 	}
   
 }
